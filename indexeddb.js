8e93c030787cd443f787f3bf91f8cfcb40f7f662
kenwoo9y <ei.1px.9.01@gmail.com>
2020-06-23 23:22:27 +0900

    set canvas width and height to 0 after addData

--- indexeddb.js
+++ indexeddb.js
@@ -174,6 +174,8 @@ function addData(event) {
         file.value = '';
         var ctx = canvas.getContext('2d');
         ctx.clearRect(0,0,canvas.width,canvas.height);
+        canvas.width = 0;
+        canvas.height = 0;
     };
 };
 
